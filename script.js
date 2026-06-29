655e3aea6b0d9e27e5351d29ba7136dc5e093534
srishcodes <srishtisaini4444@gmail.com>
2026-06-30 02:26:06 +0530

    Handle add button click event

--- script.js
+++ script.js
@@ -5,3 +5,9 @@ const taskInput = document.getElementById("taskInput");
 const addBtn = document.getElementById("addBtn");
 
 const taskList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", function(){
+
+    console.log("Button Clicked!");
+
+    });
